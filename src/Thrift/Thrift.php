f3b80d2f019437fd8ddb827a37551e0d379d7614
雷斌峰 <290887098@qq.com>
2019-02-27 17:13:58 +0800

    add Thrift class file

---
+++ src/Thrift/Thrift.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @Author: LBF 290887098@qq.com
+ * @Date:   2019-02-27 17:05:36
+ * @Last Modified by:   LBF
+ * @Last Modified time: 2019-02-27 17:05:36
+ */
+namespace Thrift;
+
+
+class Thrift{
+
+}
